b8f8c005f909a92248b14b9b3cedd0ba04e6e927
Carlos Rojas <chockreds@gmail.com>
2015-11-22 18:28:09 -0300

    Recien instale el studio en w7 y se ve mejor el esquema

---
+++ app/src/main/java/org/dinero/ganardinero/listarClientes.java
@@ -0,0 +1,10 @@
+package org.dinero.ganardinero;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by fragga on 22/11/2015.
+ */
+public class listarClientes {
+        Firebase firentes = new Firebase("https://ganardinero.firebaseio.com/clientes");
+}
