314664ec318bc93cde42776c05a6e3497c7a3a14
federico cardozo <tekoreimante@outlook.com>
2015-11-24 14:54:16 -0300

    croqueta3
    añadi un monton :)

---
+++ app/src/main/java/org/dinero/master/detalle.java
@@ -0,0 +1,32 @@
+package org.dinero.master;
+
+/**
+ * Created by tekor on 24/11/2015.
+ */
+public class detalle {
+    int id;
+    Integer idProducto;
+    Integer cantidad;
+    Integer precio;
+
+    public detalle(){}
+
+    public detalle(Integer ip, Integer c, Integer p){
+        id= ip.hashCode();
+        idProducto = ip;
+        cantidad = c;
+        precio = p;
+    }
+
+    public detalle(int i,Integer ip, Integer c, Integer p){
+        id= i;
+        idProducto = ip;
+        cantidad = c;
+        precio = p;
+    }
+    
+    public int getId(){return id;}
+    public Integer getIdProducto(){return idProducto;}
+    public Integer getCantidad(){return cantidad;}
+    public Integer getPrecio(){return precio;}
+}
